4832774bbcf92624db62196f67054264c94967d7
CarnunMP <carnun@hotmail.co.uk>
2019-10-30 14:50:41 +0000

    .use(logger) fixed

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = 'express';
+const express = require('express');
+const userDb = require('./users/userDb');
+const postsDb = require('./posts/postDb');
 
 const server = express();
-server.use(logger());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -12,6 +14,18 @@ server.get('/', (req, res) => {
 function logger(req, res, next) {
   console.log(req.method, req.url, new Date().toISOString());
   next();
+}
+
+function validateUserId(req, res, next) {
+  const { id } = req.params;
+
+  userDb.getById(id)
+    .then((resource) => {
+      console.log(resource);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 }
 
 module.exports = server;
