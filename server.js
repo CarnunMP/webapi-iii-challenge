d3c25af10206420591b7baa01aa72a3627ae945b
CarnunMP <carnun@hotmail.co.uk>
2019-10-30 15:20:31 +0000

    validateUserId done

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const postsDb = require('./posts/postDb');
 
 const server = express();
 server.use(logger);
+server.use('/users/:id', validateUserId);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
-
 function logger(req, res, next) {
   console.log(req.method, req.url, new Date().toISOString());
   next();
@@ -20,12 +20,21 @@ function validateUserId(req, res, next) {
   const { id } = req.params;
 
   userDb.getById(id)
-    .then((resource) => {
-      console.log(resource);
+    .then((user) => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({
+          message: 'invalid user id',
+        });
+      }
     })
     .catch((err) => {
-      console.log(err);
-    })
+      res.status(500).json({
+        message: 'validateUserId failed: ' + err.message,
+      });
+    });
 }
 
 module.exports = server;
